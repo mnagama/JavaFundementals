d5907a0d44b275c09b754b0f646d3cfc56242ee9
Mike <mikail149@gmail.com>
2020-04-28 15:34:08 -0500

    lesson 4 space my application overview

---
+++ src/com/fundamentals/java/House.java
@@ -0,0 +1,15 @@
+package com.fundamentals.java;
+
+public class House {
+
+
+    String foundation;
+    String roofType;
+    String dooColor;
+    int windowSize;
+
+    public void doorOpenClose ()  {
+        System.out.println("The door opens");
+            }
+
+}
--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -3,7 +3,15 @@ package com.fundamentals.java;
 public class Main {
 
     public static void main(String[] args) {
-        //write your code
+        // single line comment
         System.out.println("Hello Java");
-    }
-}
+        myHouseExample ();
+    } // end method main
+
+    /* This is a multi-line comment */
+}public static void myHouseExample () {
+    House myHouse = new House();
+    myHouse.doorOpenClose();
+} // end method myHouseExample
+
+} // end class Main
