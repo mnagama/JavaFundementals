3feebfad8cae4297d6366ed6443308de806d9cab
Mike <mikail149@gmail.com>
2020-04-29 15:22:17 -0500

    Lesson 5 Primitive Data Types

---
+++ src/com/fundamentals/java/Lesson5.java
@@ -0,0 +1,97 @@
+package com.fundamentals.java;
+/*
+* The contents of this class is for the lesson 5 slide
+* deck. Primitive Data Types & more
+* */
+public class Lesson5  {
+    // This primitive type can be true or false only
+    boolean myBoolean = true;
+
+    // This primitive type has value range of 0 to 65535
+    char myChar  = 'C';
+
+    // This primitive type has a value range of -128 to 128
+    byte myByte = 9;
+
+    // This primitive type has a value range of -32768 to 32767
+    short myShort = 235;
+
+    /* This primitive type has a value range of
+    * -2,147,483,648 to 2,147,483,647 */
+    int myInt = 2354;
+
+    /* This primitive type has a value range of
+    * -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 */
+    long myLong = 56864L;
+
+    // This primitive type has a range of 3.4e-038 to 3.4e+038
+    float myFloat =34.53F;
+    // This primitive type has a range of 1.7e-308 to 1.7e+308
+    double myDouble = 84.98D;
+    double myOtherDouble = 36.43;
+
+    // Decimal value of 26
+    int value = 26;
+
+    // Hexadecimal value 26
+    int value2 =0xla;
+
+    //Binary value of 26
+    int value3 =0b11010;
+
+    // Scientific Notation uses E or e
+    double d2 = 23.65E8;
+
+    // Using underscores to make the numberreadable
+    long myOtherLong =4_568_987_367L;
+    double dd2 = 23_345.3;
+
+    // This method sows a local variable
+    public void showLocalExample () {
+        int able = 20;
+        System.out.println(able);
+    }   // end method showLocalExample
+
+    /* This method shows Instance Variable() {
+     * inside this method */
+    public void showInstanceVarible () {
+        System.out.println("Before");
+        System.out.println(myDouble);
+        System.out.println("After");
+        myDouble = 98.6; // 84.98 original value
+        System.out.println(myDouble);
+    }  //end method showInstanceVarible
+
+      // Th is method has 2 parameters and a return type
+      public int showLocalWithReturn (int able,int type2) {
+          return able + type2;
+
+     // This method shows implicit and explicit conversion
+      public void showConversions () {
+          long ll2 = myInt; // implicit or widening conversion
+          int val = (int)myLong; // explict or narrowing cast
+          int charlie = (int)myDouble; // explicit or narrowing
+          int delta = (int)myOtherLong; // explicit, may show error }
+
+          System.out.println(ll2);
+          System.out.println(val);
+          System.out.println(charlie);
+          System.out.println(delta);
+
+          public static void lesson5Example() {
+              Lesson5 myFive = new Lesson5();
+              myFive.showLocalExample();
+              int example = myFive.showLocalWithinReturn(able:5,type2:7); // return toa variable
+              System.out.println(example); //print variable from line above
+              System.out.println(myFive.showLocalWithReturn(able:2,type2:9)); //direct return ofin:
+              myFive.showInstanceVariable();
+              myFive.showConversions();
+
+
+
+          // end class lesson5
+
+
+}
+
+
--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -3,15 +3,21 @@ package com.fundamentals.java;
 public class Main {
 
     public static void main(String[] args) {
-        // single line comment
-        System.out.println("Hello Java");
-        myHouseExample ();
+        // System.out.println("Hello Java");
+        // System.out.println(
+        // This is my first sentence
+
+        // System.out.println("This is my first sentence") +
+        //      ("I am new to Java")+
+        //    ("This Java course is cool") +
+        //  ("I am learning stuff every day");
+        // myHouseExample ();
     } // end method main
 
     /* This is a multi-line comment */
-}public static void myHouseExample () {
-    House myHouse = new House();
-    myHouse.doorOpenClose();
-} // end method myHouseExample
+    public static void myHouseExample() {
+        House myHouse = new House();
+        myHouse.doorOpenClose();
+    } // end method myHouseExample
 
 } // end class Main
